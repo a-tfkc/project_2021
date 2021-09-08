51e9179db945bb6106428d49a8bd43f57d8609ab
a-tfkc <aytentuefekci@web.de>
2021-09-08 21:51:23 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Logo" src="./assets/logo.png" width="200" height="200">
+    <Message msg="Welcome!"/>
+  </div>
+</template>
+
+<script>
+import Message from "./components/Message";
+
+export default {
+  name: 'App',
+  components: {
+    Message
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
