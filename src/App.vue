fbb9ba32452e3b8fc2fd36e0975c07e5c6016b3f
a-tfkc <aytentuefekci@web.de>
2022-01-18 23:16:31 +0100

    started working on second navbar version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div id="app">
-    <NavBar1></NavBar1>
+    <NavBar2></NavBar2>
     <Slider1></Slider1>
   </div>
 
@@ -9,13 +9,13 @@
 
 <script>
 
-import NavBar1 from "./components/navbars/NavBar1";
+import NavBar2 from "./components/navbars/NavBar2";
 import Slider1 from "./components/sliders/Slider1";
 
 export default {
   name: 'App',
   components: {
-    NavBar1,
+    NavBar2,
     Slider1,
   }
 }
